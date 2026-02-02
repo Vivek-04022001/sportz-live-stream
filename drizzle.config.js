cc5c0940b28519d17578325b05f8ffffdcdeb7e3
Vivek <vk285090@gmail.com>
2026-02-02 07:56:35 +0530

    setup db

---
+++ drizzle.config.js
@@ -0,0 +1,14 @@
+import "dotenv/config";
+
+if (!process.env.DATABASE_URL) {
+  throw new Error("DATABASE_URL is not set in .env file");
+}
+
+export default {
+  schema: "./src/db/schema.js",
+  out: "./drizzle",
+  dialect: "postgresql",
+  dbCredentials: {
+    url: process.env.DATABASE_URL,
+  },
+};
